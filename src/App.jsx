b60e7a13219c51679bff5c87502c29029472614f
PITT1 <petroleo.plta.97@gmail.com>
2024-03-31 11:58:35 -0400

    mejorando la interfaz

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import mist from "../public/mist.png";
 import snow from "../public/snow.png";
 import rain from "../public/rain.png";
 
-let city = ""
+let city = "";
 
 function App() {
   const [temp, setTemp] = useState(null);
   const [hum, setHum] = useState(null);
   const [image, setImage] = useState("");
+  const [descrip, setDescrip] = useState("");
+  const [wind, setWind] = useState("");
 
   const handleTextChange = (newText) => {
     city = newText;
@@ -22,8 +24,10 @@ function App() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=c1b7a79e7381e224e8a6e76647de2e49`)
     .then(res => res.json())
     .then(res => {
-      setTemp(res.main.temp);
+      setTemp(Math.trunc(res.main.temp));
       setHum(res.main.humidity);
+      setDescrip(res.weather[0].description);
+      setWind(res.wind.speed);
 
       switch(res.weather[0].main){
         case "Clear":
@@ -58,9 +62,15 @@ function App() {
           <div className='img-weather'>
             {temp && <img src={image} height={150} alt="imagen que representa un clima" />}
           </div>
+          <div className='constainer-temp'>
+          {temp && <p className='temp'>{temp}</p>}
+          </div>
+          <div className='constainer-descript'>
+            {temp && <p className='description'>{descrip}</p>}
+          </div>
           <div className='container-magnitud'>
-            {temp && <p className='mag'>Temp: {temp}°C</p>}
-            {hum && <p className='mag'>Hum: {hum}%</p>}
+            {hum && <p className='humedad'>{hum}%</p>}
+            {hum && <p className='wind-speed'>{wind}Km/h</p>}
           </div>
         </section>
       </div>
